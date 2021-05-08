e3eee736e1e56a6ccd1448779f9d41b61c024543
zbeach <z.beach@icloud.com>
2021-05-08 00:03:03 -0400

    Fix credits and typo

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const apiKey = 'API_KEY';
   for (let moreVideos = true, i = 0; moreVideos; i += limit) {
     console.log(`Getting videos ${i}-${i + limit - 1}...`);
     const videos = await getVideosInfo(i, limit);
-    console.log(`Downloading videos ${i}=${i + limit - 1}...`);
+    console.log(`Downloading videos ${i}-${i + limit - 1}...`);
     await downloadVideos(videos);
     await sleep(18000);
   }
@@ -59,7 +59,7 @@ async function downloadVideo(video, destination) {
     }
     
     // Borrowed from @lightpohl and modified a bit
-    // [gb-dl](https://github.com/lightpohl/gb-dl/blob/cafba731cfdea6afd8917b0cdbb0fca42c486c5c/bin/util.js#L428)
+    // https://github.com/lightpohl/gb-dl/blob/cafba731cfdea6afd8917b0cdbb0fca42c486c5c/bin/util.js#L428
     const filePath = `${showPath}/${filenamify(video.name, { replacement: '' })}.mp4`;
     try {
       await pipeline(
@@ -88,7 +88,7 @@ async function downloadVideo(video, destination) {
 }
 
 // Borrowed from @lightpohl
-// [gb-dl](https://github.com/lightpohl/gb-dl/blob/cafba731cfdea6afd8917b0cdbb0fca42c486c5c/bin/util.js#L322)
+// https://github.com/lightpohl/gb-dl/blob/cafba731cfdea6afd8917b0cdbb0fca42c486c5c/bin/util.js#L322
 function printProgress({ percent, total, transferred }) {
   const BYTES_IN_MB = 1000000;
 
@@ -115,7 +115,7 @@ function printProgress({ percent, total, transferred }) {
 };
 
 // Borrowed from @lightpohl
-// [gb-dl](https://github.com/lightpohl/gb-dl/blob/cafba731cfdea6afd8917b0cdbb0fca42c486c5c/bin/util.js#L466)
+// https://github.com/lightpohl/gb-dl/blob/cafba731cfdea6afd8917b0cdbb0fca42c486c5c/bin/util.js#L466
 function getHighestQualityVideoUrl(video) {
   const qualities = [ 'hd', 'high', 'low', 'mobile' ];
   for (quality of qualities) {
